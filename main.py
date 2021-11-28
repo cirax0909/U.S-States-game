f4e320fa83aca1f02ae0deecd7a22e38afbc8979
cirax0909 <62638678+cirax0909@users.noreply.github.com>
2021-11-27 21:45:42 -0500

    add create csv file method in main

--- main.py
+++ main.py
@@ -13,17 +13,25 @@ already_taken = []
 display = Display()
 
 while right_state <= 50:
-    answer_state = screen.textinput(title=f"Correct answer {right_state}/50", prompt="Enter a state")
-    already_taken.append(answer_state)
-    print(already_taken)
+    answer_state = screen.textinput(title=f"Correct answer {right_state}/50", prompt="Enter a state").title()
     states_file = pandas.read_csv("50_states.csv")
-    us_states = states_file.state
+    us_states = states_file.state.to_list()
+    if answer_state == "Exit":
+        missing_state = []
+        for state in us_states:
+            if state not in already_taken:
+                missing_state.append(state)
+                new_data = pandas.DataFrame(missing_state)
+                new_data.to_csv("missing states.csv")
+        break
 
     for state in us_states:
-        if answer_state not in already_taken and answer_state == state:
+        if answer_state == state:
             a = int(states_file.x[states_file["state"] == answer_state])
             b = int(states_file.y[states_file.state == answer_state])
             display.display(a, b, answer_state)
-            right_state += 1
+            if answer_state not in already_taken:
+                right_state += 1
+                already_taken.append(answer_state)
 
 screen.exitonclick()
